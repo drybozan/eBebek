9e0287faf90f796aece927ed9f0cfc444ce65c44
drybozan <derya7649@gmail.com>
2022-11-02 17:47:36 +0300

    vücut kitle indeksi hesaplama eklendi

---
+++ src/VucutKitleİndeksHesaplama/VucutKitleIndexHesapla.java
@@ -0,0 +1,19 @@
+package VucutKitleİndeksHesaplama;
+
+import java.util.Scanner;
+
+public class VucutKitleIndexHesapla {
+    public static void main(String[] args) {
+        double kg , m , Vindex;
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Lütfen boyunuzu (metre cinsinde) giriniz: ");
+        m = scan.nextDouble();
+
+        System.out.println("Lütfen kilonuzu giriniz :" );
+        kg = scan.nextDouble();
+
+        Vindex = kg/(m*m);
+        System.out.println("Vücut Kitle İndeksiniz :" + Vindex);
+    }
+}
