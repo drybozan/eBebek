91f2b94a46ec8d35861434e126bae4fcf9639b50
drybozan <derya7649@gmail.com>
2022-11-02 18:09:03 +0300

    manav kasa hesaplama eklendi

---
+++ src/ManavKasaProgramı/ManavKasaProgrami.java
@@ -0,0 +1,30 @@
+package ManavKasaProgramı;
+
+import java.util.Scanner;
+
+public class ManavKasaProgrami {
+    public static void main(String[] args) {
+        double armut =2.14, elma =3.67, domates=1.11, muz = 0.95, patlican = 5.0;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Armut Kaç Kilo ? :");
+        double armutKg = scanner.nextDouble();
+
+        System.out.println("Elma Kaç Kilo ? :");
+        double elmaKg = scanner.nextDouble();
+
+        System.out.println("Domates Kaç Kilo ? :");
+        double domatesKg = scanner.nextDouble();
+
+        System.out.println("Muz Kaç Kilo ? :");
+        double muzKg = scanner.nextDouble();
+
+        System.out.println("Patlıcan Kaç Kilo ? :");
+        double patlicanKg = scanner.nextDouble();
+
+        double toplam = (armut*armutKg)+(elma*elmaKg)+(domates*domatesKg)+(muz*muzKg)+(patlican*patlicanKg);
+
+        System.out.println("Toplam tutar :" +toplam);
+    }
+}
