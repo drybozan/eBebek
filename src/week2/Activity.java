d1ac3be53ca75ef0923e31775c13af4a53fddd1e
drybozan <derya7649@gmail.com>
2022-11-09 20:23:37 +0300

    commit for week2

---
+++ src/week2/Activity.java
@@ -0,0 +1,26 @@
+package week2;
+
+import java.util.Scanner;
+
+public class Activity {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the air temperature : ");
+        int heat = input.nextInt();
+
+        if (heat<5){
+            System.out.println("Do skiing.");
+        } else if (heat>5 && heat<15) {
+            System.out.println("Go to the movies.");
+
+        } else if (heat>15 && heat<25) {
+            System.out.println("Picnic.");
+
+        } else if (heat>25) {
+            System.out.println("Go swimming.");
+
+        }
+
+    }
+}
