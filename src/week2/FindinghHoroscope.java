0a5fe2fff7c35ce932f48395c404ec1d1e1e5532
drybozan <derya7649@gmail.com>
2022-11-11 11:02:54 +0300

    commit for week2

---
+++ src/week2/FindinghHoroscope.java
@@ -0,0 +1,7 @@
+package week2;
+
+public class FindinghHoroscope {
+    public static void main(String[] args) {
+
+    }
+}
--- src/week2/NumberSorting.java
+++ src/week2/NumberSorting.java
@@ -13,29 +13,29 @@ public class NumberSorting {
         num2= scanner.nextInt();
         num3= scanner.nextInt();
 
-        if(num1>num2 && num1>num3){
-            if(num2>num3)
-                System.out.println(num1+">"+ num2 +">"+num3);
+        if(num1<num2 && num1<num3){
+            if(num2<num3)
+                System.out.println(num1+"<"+ num2 +"<"+num3);
             else if(num2==num3)
-                System.out.println(num1+">"+num2+"="+num3);
+                System.out.println(num1+"<"+num2+"="+num3);
             else
-                System.out.println(num1+">"+num3+">"+num2);
+                System.out.println(num1+"<"+num3+"<"+num2);
         }
-        else if(num2>num1 && num2>num3){
-            if(num1>num3)
-                System.out.println(num2+">"+num1+">"+num3);
+        else if(num2<num1 && num2<num3){
+            if(num1<num3)
+                System.out.println(num2+"<"+num1+"<"+num3);
             else if(num1==num3)
-                System.out.println(num2+">"+num1+"="+num3);
+                System.out.println(num2+"<"+num1+"="+num3);
             else
-                System.out.println(num2+">"+num3+">"+num1);
+                System.out.println(num2+"<"+num3+"<"+num1);
         }
-        else if(num3>num2 && num3>num1){
-            if(num2>num1)
-                System.out.println(num3+">"+num2+">"+num1);
+        else if(num3<num2 && num3<num1){
+            if(num2<num1)
+                System.out.println(num3+"<"+num2+"<"+num1);
             else if(num2==num1)
-                System.out.println(num3+">"+num2+"="+num1);
+                System.out.println(num3+"<"+num2+"="+num1);
             else
-                System.out.println(num3+">"+num2+">"+num1);
+                System.out.println(num3+"<"+num2+"<"+num1);
         }else{
             System.out.println(num1+" = "+num2+" = "+num3);
         }
