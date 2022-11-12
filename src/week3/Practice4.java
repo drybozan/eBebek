307487a7252bbcb11b5817b28d2238e3343c14c4
drybozan <derya7649@gmail.com>
2022-11-13 01:16:24 +0300

    commit for week3

---
+++ src/week3/Practice4.java
@@ -0,0 +1,35 @@
+package week3;
+
+import java.util.Scanner;
+
+public class Practice4 {
+    public static void main(String[] args) {
+        //  C(n,r) = n! / (r! * (n-r)!)
+
+        Scanner input = new Scanner(System.in);
+
+        int n,r,combination,nTotal=1,rTotal=1,nMinusR=1;
+
+        System.out.print("Eleman Sayısı (n) : ");
+        n = input.nextInt();
+
+        System.out.print("Seçim Sayısı (r) : ");
+        r = input.nextInt();
+
+        for(int i=1; i<=n; i++){
+            nTotal *= i;
+        }
+
+        for(int j=1; j<=r; j++){
+            rTotal *= j;
+        }
+
+        for(int k=1; k<=(n-r); k++){
+            nMinusR *= k;
+        }
+
+        combination = nTotal / (rTotal * nMinusR);
+        System.out.println("C("+n+","+r+") kombinasyonu = "+combination);
+    }
+    }
+
