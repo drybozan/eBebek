81f9030ba8064a0c3dd1ff8af80c41453815a428
drybozan <derya7649@gmail.com>
2022-11-12 12:20:02 +0300

    commit for week2

---
+++ src/week2/ChineseZodiac.java
@@ -0,0 +1,59 @@
+package week2;
+
+import java.util.Scanner;
+
+public class ChineseZodiac {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        //Kullanicidan dogum yili alinir
+        System.out.println("Lütfen doğum yılınızı giriniz:");
+        int year = scan.nextInt();
+
+        //Dogum yilinin 12 ile bolumu bulunur
+        int zodiac = year%12;
+
+        //Zodyak yili enizi ekrana yazdirir
+        switch (zodiac){
+
+            case 0 :
+                System.out.println("Cin Zodyagın : Maymun");
+                break;
+            case 1 :
+                System.out.println("Cin Zodyagın : Horoz");
+                break;
+            case 2 :
+                System.out.println("Cin Zodyagın : Kopek");
+                break;
+            case 3 :
+                System.out.println("Cin Zodyagın : Domuz");
+                break;
+            case 4 :
+                System.out.println("Cin Zodyagın : Fare");
+                break;
+            case 5 :
+                System.out.println("Cin Zodyagın : Okuz");
+                break;
+            case 6 :
+                System.out.println("Cin Zodyagın : Kaplan");
+                break;
+            case 7 :
+                System.out.println("Cin Zodyagın : Tavsan");
+                break;
+            case 8 :
+                System.out.println("Cin Zodyagın : Ejderha");
+                break;
+            case 9 :
+                System.out.println("Cin Zodyagın : Yılan");
+                break;
+            case 10 :
+                System.out.println("Cin Zodyagın : At");
+                break;
+            case 11 :
+                System.out.println("Cin Zodyagın : Koyun");
+                break;
+        }
+
+    }
+}
