04159a2271be0bb4d09a4823c628dffc67571dde
drybozan <derya7649@gmail.com>
2022-11-13 01:05:33 +0300

    commit for week3

--- src/week3/Practice3.java
+++ src/week3/Practice3.java
@@ -1,7 +1,33 @@
 package week3;
 
-public class Practice3 {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
-    }
+public class Practice3 {
+        public static void main(String[] args) {
+
+            Scanner scan=new Scanner(System.in);
+
+            int number,firstTotal=1,secondTotal=1;
+
+            System.out.print("Sayı giriniz : ");
+            if(scan.hasNextInt()) {
+                number = scan.nextInt();
+
+                System.out.print("4 un kuvvetleri: ");
+                while(firstTotal<number){
+                    System.out.print(firstTotal+" ");
+                    firstTotal*=4;
+
+                }
+                System.out.print("\n 5 in kuvvetleri : ");
+
+                while (secondTotal<number){
+                    System.out.print(secondTotal+" ");
+
+                    secondTotal*=5;
+                }
+
+            }
+        }
+
 }
