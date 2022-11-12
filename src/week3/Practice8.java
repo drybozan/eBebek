eeca377ac79e20829f00b45b9fb67cc179dd5e94
drybozan <derya7649@gmail.com>
2022-11-13 02:00:52 +0300

    commit for week3

---
+++ src/week3/Practice8.java
@@ -0,0 +1,30 @@
+package week3;
+
+import java.util.Scanner;
+
+public class Practice8 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n;
+        System.out.print("Elmas için n sayısını giriniz: ");
+        n = scanner.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i) - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int z = 1; z <= n; z++) {
+            for (int m =1; m<=z ; m++) {
+                System.out.print(" ");
+            }
+            for (int b =1; b<=(2*n-2*z-1); b++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
