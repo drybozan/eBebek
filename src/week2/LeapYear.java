1b3db7e4dd3daf753d93ee29815d8d9c3fcfaf2b
drybozan <derya7649@gmail.com>
2022-11-12 12:39:34 +0300

    commit for week2

--- src/week2/LeapYear.java
+++ src/week2/LeapYear.java
@@ -4,18 +4,21 @@ import java.util.Scanner;
 
 public class LeapYear {
     public static void main(String[] args) {
+
+        //Java ile kullanıcının girdiği yılın artık olup olmadığını bulan program
         Scanner scan = new Scanner(System.in);
         System.out.println("Yıl giriniz :");
         int year = scan.nextInt();
 
         boolean artik = false;
-
+ // yıl 100' e kalansız bölünüyor mu
        if(year % 100 ==0){
+           // yıl 400' e kalansız bölünüyor mu
            if(year % 400 == 0)
-               artik= true;
-
+               artik= true; //bölünüyorsa artık yıl
+     // yıl 4' e kalansız bölünüyor mu
        }else if (year % 4 ==0){
-           artik = true;
+           artik = true; //bölünüyorsa artık yıl
        }else {
            System.out.println(" Girdiğiniz bilgileri kontrol edin.");
        }
